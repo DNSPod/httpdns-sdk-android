682693f98c3231fca25e5d169351c8e6eaefe36b
lemondark <743855454@qq.com>
2022-08-31 20:27:03 +0800

    fix: 优化代码

--- src/main/java/com/tencent/msdk/dns/core/cache/database/LookupCache.java
+++ src/main/java/com/tencent/msdk/dns/core/cache/database/LookupCache.java
@@ -4,9 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
-import androidx.room.TypeConverters;
 
-import com.tencent.msdk.dns.base.log.DnsLog;
 import com.tencent.msdk.dns.core.LookupResult;
 import com.tencent.msdk.dns.core.rest.share.AbsRestDns;
 
@@ -29,7 +27,10 @@ public class LookupCache {
 
     public boolean isExpired() {
         AbsRestDns.Statistics stat = (AbsRestDns.Statistics) lookupResult.stat;
-        return System.currentTimeMillis() > stat.expiredTime;
+        if (stat != null) {
+            return System.currentTimeMillis() > stat.expiredTime;
+        }
+        return true;
     }
 }
 
