d7892355eec7c0fbbdfe6edb0a591adc63f77fd2
lemondark <743855454@qq.com>
2023-03-27 17:33:52 +0800

    feat: room版本升级成androidx&无用代码去除

--- src/main/java/com/tencent/msdk/dns/core/cache/database/LookupCache.java
+++ src/main/java/com/tencent/msdk/dns/core/cache/database/LookupCache.java
@@ -22,9 +22,6 @@ public class LookupCache {
         this.lookupResult = mLookupResult;
     }
 
-    public LookupCache() {
-    }
-
     public boolean isExpired() {
         AbsRestDns.Statistics stat = (AbsRestDns.Statistics) lookupResult.stat;
         if (stat != null) {
