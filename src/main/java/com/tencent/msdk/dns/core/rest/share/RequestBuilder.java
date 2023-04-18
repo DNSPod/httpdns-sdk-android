997506152b882a393df0544a63ba5238a8de7382
lemondark <743855454@qq.com>
2023-04-18 15:41:55 +0800

    feat: 新增ecs ip配置

--- src/main/java/com/tencent/msdk/dns/core/rest/share/RequestBuilder.java
+++ src/main/java/com/tencent/msdk/dns/core/rest/share/RequestBuilder.java
@@ -2,18 +2,19 @@ package com.tencent.msdk.dns.core.rest.share;
 
 import android.text.TextUtils;
 
+import com.tencent.msdk.dns.DnsService;
 import com.tencent.msdk.dns.core.Const;
 
 import java.util.Locale;
 
 public final class RequestBuilder {
 
-    private static final String INET_REQUEST_FORMAT = "dn=%s&clientip=1&ttl=1&id=%s&alg=%s";
-    private static final String INET6_REQUEST_FORMAT = "dn=%s&clientip=1&ttl=1&id=%s&alg=%s&type=aaaa";
-    private static final String DOUB_REQUEST_FORMAT = "dn=%s&clientip=1&ttl=1&id=%s&alg=%s&type=addrs";
-    private static final String HTTPS_INET_REQUEST_FORMAT = "dn=%s&clientip=1&ttl=1&id=%s&token=%s";
-    private static final String HTTPS_INET6_REQUEST_FORMAT = "dn=%s&clientip=1&ttl=1&id=%s&token=%s&type=aaaa";
-    private static final String HTTPS_DOUB_REQUEST_FORMAT = "dn=%s&clientip=1&ttl=1&id=%s&token=%s&type=addrs";
+    private static final String INET_REQUEST_FORMAT = "dn=%s&ip=%s&clientip=1&ttl=1&id=%s&alg=%s";
+    private static final String INET6_REQUEST_FORMAT = "dn=%s&ip=%s&clientip=1&ttl=1&id=%s&alg=%s&type=aaaa";
+    private static final String DOUB_REQUEST_FORMAT = "dn=%s&ip=%s&clientip=1&ttl=1&id=%s&alg=%s&type=addrs";
+    private static final String HTTPS_INET_REQUEST_FORMAT = "dn=%s&ip=%s&clientip=1&ttl=1&id=%s&token=%s";
+    private static final String HTTPS_INET6_REQUEST_FORMAT = "dn=%s&ip=%s&clientip=1&ttl=1&id=%s&token=%s&type=aaaa";
+    private static final String HTTPS_DOUB_REQUEST_FORMAT = "dn=%s&ip=%s&clientip=1&ttl=1&id=%s&token=%s&type=addrs";
 
     public static String buildInetRequest(String encryptedHostname, String bizId, String alg) {
         return buildRequest(encryptedHostname, bizId, alg, INET_REQUEST_FORMAT);
@@ -39,8 +40,9 @@ public final class RequestBuilder {
         if (TextUtils.isEmpty(encryptedHostname)) {
             return "";
         }
+        String ip = DnsService.getDnsConfig().routeIp;
         return String.format(Locale.US,
-                format, encryptedHostname, bizId, alg);
+                format, encryptedHostname, ip, bizId, alg);
     }
 
     public static String buildHttpsInetRequest(String encryptedHostname, String bizId, String token) {
@@ -67,7 +69,8 @@ public final class RequestBuilder {
         if (TextUtils.isEmpty(encryptedHostname)) {
             return "";
         }
+        String ip = DnsService.getDnsConfig().routeIp;
         return String.format(Locale.US,
-                format, encryptedHostname, bizId, token);
+                format, encryptedHostname, ip, bizId, token);
     }
 }
