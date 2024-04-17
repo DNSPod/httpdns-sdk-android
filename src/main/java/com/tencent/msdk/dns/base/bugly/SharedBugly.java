50cad7d0c69fcf7584a781f8982969a2af7f8c99
sarahzzhang <sarahzzhang@tencent.com>
2024-04-17 17:11:10 +0800

    fix: 关闭时处理缓存问题，彻底关掉共享式Bugly上报

--- src/main/java/com/tencent/msdk/dns/base/bugly/SharedBugly.java
+++ src/main/java/com/tencent/msdk/dns/base/bugly/SharedBugly.java
@@ -14,13 +14,19 @@ public class SharedBugly {
     public static void init(Context context) {
         try {
             if (DnsService.getDnsConfig().experimentalBuglyEnable) {
-                SharedPreferences settings = context.getSharedPreferences("BuglySdkInfos", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = settings.edit();
-                editor.putString(appId, appVersion);//必填信息
+                SharedPreferences sharedPreferences = context.getSharedPreferences("BuglySdkInfos", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(appId, appVersion); //必填信息
                 editor.commit();
                 DnsLog.d("shared bugly inited success");
             } else {
-
+                SharedPreferences sharedPreferences = context.getSharedPreferences("BuglySdkInfos",
+                        Context.MODE_PRIVATE);
+                if (sharedPreferences.contains(appId)) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.remove(appId);
+                    editor.apply();
+                }
             }
         } catch (Exception e) {
             DnsLog.d("shared bugly inited error " + e);
